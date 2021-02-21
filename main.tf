8f6c44677d6562be31308f68d1404eacc1ef598a
Harshwardhan Singh <ramgarhia.singh99@gmail.com>
2021-02-21 22:05:40 +0530

    Added Azure Storage backend and updated workflow to select tf workspace

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@
       version = "3.0.1"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "TerraformBackend-RG"
+    storage_account_name = "rs9tfbackendstorageacc"
+    container_name = "default"
+    key = "terraform.tfstate"
+    access_key = "Q7yphjXaTEvXp0mAe661QXCXLlhAcDp0U/iszZPen0Pt/hYc3LqXztceq1xsSakcT7Xg9RkOamODuBpyhZQKlQ=="
+  }
 }
 
 provider "random" {
