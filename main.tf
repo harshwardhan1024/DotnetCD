071d2330152f19d53eca3e40adacb02b427f9815
Harshwardhan Singh <ramgarhia.singh99@gmail.com>
2021-02-26 22:13:06 +0530

    Updated workflow file

--- main.tf
+++ main.tf
@@ -7,11 +7,7 @@
   }
 
   backend "azurerm" {
-    resource_group_name = "TerraformBackend-RG"
-    storage_account_name = "rs9tfbackendstorageacc"
-    container_name = "default"
     key = "terraform.tfstate"
-    access_key = "Q7yphjXaTEvXp0mAe661QXCXLlhAcDp0U/iszZPen0Pt/hYc3LqXztceq1xsSakcT7Xg9RkOamODuBpyhZQKlQ=="
   }
 }
 
